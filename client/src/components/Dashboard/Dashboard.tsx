b2a2e0b8b43d9f02ac6f40c643a4f295d683788a
gonzalo <gonzlaov2000@gmail.com>
2025-02-02 20:04:47 +0100

    Dashboard started

---
+++ client/src/components/Dashboard/Dashboard.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import styles from './Dashboard.module.css'
+import Navbar from '../navbar/Navbar'
+import Footer from '../Footer/Footer'
+
+const Dashboard = () => {
+  return (
+    <>
+
+    </>
+  )
+}
+
+export default Dashboard

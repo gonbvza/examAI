eef11cc6630d93cf5ddc3344a0460a625896e4d2
gonzalo <gonzlaov2000@gmail.com>
2025-03-02 14:46:05 +0100

    hosting

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // Allows Railway to bind to the correct network interface
+    port: 4173, // Uses Railway's assigned port or defaults to 4173
+  }
 })

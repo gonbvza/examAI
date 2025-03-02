928ff9441ad0b456a24543e085b4c9a2be3911da
gonbvza <gonzalov2000@gmail.com>
2025-03-02 18:50:37 +0100

    vite config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -7,8 +7,15 @@ export default defineConfig({
   server: {
     host: '0.0.0.0', // Allows Railway to bind to the correct network interface
     port: 8080, // Uses Railway's assigned port or defaults to 4173
+    proxy: {
+      "/api": {
+        target: "http://0.0.0.0:8000", // Change this to your Django backend's Railway URL in production
+        changeOrigin: true,
+        secure: false,
+      },
+    },
   },
   preview: {
-    allowedHosts: ['examai-production.up.railway.app', 'localhost', '0.0.0.0'], // Add your Railway URL here
+    allowedHosts: ['examai-production.up.railway.app', 'localhost', '0.0.0.0'],
   }
-})
+});

48f0226a3ff3ae125938e39f0f3bb39247b9f84a
gonzalo <gonzlaov2000@gmail.com>
2025-03-02 14:41:02 +0100

    package

--- server/server/serverDjango/settings.py
+++ server/server/serverDjango/settings.py
@@ -128,6 +128,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Add this to serve the React build
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, '../../client/build/static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field

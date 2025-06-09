26e3cb77a2c1fb7c8a9fe633db489bc4d9cd5e9c
gonbvza <gonzalov2000@gmail.com>
2025-06-09 12:08:57 +0200

    nginx fixed

--- server/entrypoint.sh
+++ server/entrypoint.sh
@@ -4,5 +4,4 @@ echo "Running migrations..."
 python manage.py migrate
 
 echo "Starting Django server..."
-python manage.py runserver 0.0.0.0:8000
-
+gunicorn serverDjango.wsgi:application --bind "0.0.0.0:8000" --workers 4
